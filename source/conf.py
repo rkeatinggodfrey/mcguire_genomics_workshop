56aaa899f001b6b197c1ce1ec24efca10b3947d3
Rebekah Keating Godfrey <115112401+rkeatinggodfrey@users.noreply.github.com>
2026-06-07 00:28:11 -0400

    readthedocs update

--- source/conf.py
+++ source/conf.py
@@ -13,7 +13,10 @@ author = 'Ivone de Bem Oliveira, Bert Foquet, R Keating Godfrey, Taylor Pierson'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    ...
+    'sphinx_rtd_theme',
+    ]
 
 templates_path = ['_templates']
 exclude_patterns = []
